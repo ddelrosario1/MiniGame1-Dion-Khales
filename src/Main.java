f0e2195ce65051275b5942170abeac45d7443550
khales <mrahman10@ggc.edu>
2023-06-05 20:24:21 -0400

    Added Main.java and RoomList.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+        Game theGame = new Game();
+        theGame.play();
+        System.out.println("Thank you for playing!");
     }
 }
--- src/RoomList.java
+++ src/RoomList.java
@@ -1,2 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class RoomList {
+    private ArrayList<Room> roomArray = new ArrayList<>();
+
+    // Turns out that nextInt() does weird things when used with \n and doesn't recognize line breaks so it does not
+    // let you read the next int and instead throws type mismatch exceptions
+    // Did not work: delimiter should cause it to ignore the appropriate parts in the text file that allow human reading ease
+    public RoomList() throws FileNotFoundException {
+        Scanner in = new Scanner(new File("Rooms.txt"));
+
+        while (in.hasNext()) {
+            String name = in.nextLine();
+            int id = Integer.parseInt(in.nextLine());
+            String desc = in.nextLine();
+            int north = Integer.parseInt(in.nextLine());
+            int east = Integer.parseInt(in.nextLine());
+            int south = Integer.parseInt(in.nextLine());
+            int west = Integer.parseInt(in.nextLine());
+
+            Room newRoom = new Room(name, id, desc, north, east, south, west);
+            roomArray.add(newRoom);
+        }
+    }
+
+    public ArrayList<Room> getRoomArray() {
+        return roomArray;
+    }
 }
